386ee9b660e0ff58c00c142a2927cf83d623d8f2
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-12-25 20:17:53 -0500

    Create createProject Module

---
+++ src/createProject.js
@@ -0,0 +1,6 @@
+
+function createProject(name="Project") {
+    return { name };
+};
+
+export { createProject };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import './style.css';
 import { createTodo } from './createTodo.js';
+import { createProject } from './createProject.js';
 
 
 console.log(createTodo('test', '2', '3', '4', '5'));
+console.log(createProject('test'));
+
+let test = createProject('ddf');
+test.todo1 = { title: 'fake todo object' };
+console.log(test);
+
+
+
+/*
+
+All 'Projects' are Objects that Hold 'Todos'
+All 'Todos' are Objects
+
+*/
